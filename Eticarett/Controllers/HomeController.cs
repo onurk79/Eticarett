fe6d4315837cb1c031972e17f3e51125935cd2e2
onurk79 <gezgin.779@hotmail.com>
2016-08-22 20:15:01 +0300

    fiyat

--- Eticarett/Controllers/HomeController.cs
+++ Eticarett/Controllers/HomeController.cs
@@ -14,7 +14,26 @@ namespace Eticarett.Controllers
 {
     public class HomeController : Controller
     {
-       
+        public decimal FiyatHesap(decimal alıs, float karOranı, float kdvOranı, float indirimOranı)
+        {
+            decimal kar = alıs * Convert.ToDecimal(karOranı) / 100;
+            decimal kdv = (kar + alıs) * Convert.ToDecimal(kdvOranı) / 100;
+            decimal fiyat = kar + kdv + alıs;
+            decimal indirim = fiyat * Convert.ToDecimal(indirimOranı) / 100;
+            decimal indrimlifiyat = fiyat - indirim;
+            return indrimlifiyat;
+
+        }
+        public decimal FiyatHesap(decimal alıs, float karOranı, float kdvOranı)
+        {
+
+            decimal kar = alıs * Convert.ToDecimal(karOranı) / 100;
+            decimal kdv = (kar + alıs) * Convert.ToDecimal(kdvOranı) / 100;
+            decimal fiyat = kar + kdv + alıs;
+
+            return fiyat;
+
+        }
 
         public List<DigerUrunler> KampanyalıUrn(int page)
         {
@@ -26,36 +45,34 @@ namespace Eticarett.Controllers
             DigerUrunler urrun = new DigerUrunler();
             foreach (var _urun in currentPosts)
             {
-                urrun.Fiyat = (((_urun.UrunFiyat.AlisFiyati) *
-                       (Convert.ToDecimal(_urun.UrunFiyat.KarOranı * _urun.UrunFiyat.KdvOrani))) / 100)
-                       * Convert.ToDecimal(_urun.IndrimOranı);
+                urrun.Fiyat = FiyatHesap(_urun.UrunFiyat.AlisFiyati, _urun.UrunFiyat.KarOranı, _urun.UrunFiyat.KdvOrani, _urun.IndrimOranı);
                 MemoryStream ms = new MemoryStream(_urun.UrunFiyat.UrunDetaylari.resim, 0, _urun.UrunFiyat.UrunDetaylari.resim.Length);
                 ms.Write(_urun.UrunFiyat.UrunDetaylari.resim, 0, _urun.UrunFiyat.UrunDetaylari.resim.Length);
                 WebImage image = new WebImage(ms);
                 image.Resize(484, 441, false);
                 var upload = "~\\images\\Database\\" + _urun.UrunFiyat.UrunId + 484 + 441;
-                urrun.ResimYolu = "/images/Database/" + _urun.UrunFiyat.UrunId +484+441+ "." + image.ImageFormat;
+                urrun.ResimYolu = "/images/Database/" + _urun.UrunFiyat.UrunId + 484 + 441 + "." + image.ImageFormat;
                 image.Save(upload);
                 urrun.UrunId = _urun.UrunFiyat.UrunId;
                 urrun.UrunAdi = _urun.UrunFiyat.UrunDetaylari.model_cins;
                 b.Add(urrun);
-                
-                
+
+
             }
             return b;
         }
 
         private static int postsPerPage = 5;
         private Entitie context = new Entitie();
-        public ActionResult Index( int page=1)
+        public ActionResult Index(int page = 1)
         {
             Lists MarkaKategori = new Lists();
-           IList< Urunler> Urunler = context.Urunler.OrderByDescending(p => p.Id).Take(10).ToList();
-         
-            
-          
+            IList<Urunler> Urunler = context.Urunler.OrderByDescending(p => p.Id).Take(10).ToList();
+
+
+
             List<Marka> _markalist = new List<Marka>();
-          
+
             IList<Markalar> Markalar = context.Markalar.ToList();
             foreach (var marka in Markalar)
             {
@@ -73,16 +90,16 @@ namespace Eticarett.Controllers
                 _marka.imagePath = "/images/Database/" + _marka.Id + "." + image.ImageFormat;
                 image.Save(upload);
             }
-        
+
             var totalPostCount = context.Kampanya.Count();
 
-           var currentPosts= KampanyalıUrn(page);
+            var currentPosts = KampanyalıUrn(page);
 
-           
+
             return View(new KampanyalıUrunler()
             {
-                kampanyalıUrunler=new PageData<DigerUrunler>(currentPosts, totalPostCount, page, postsPerPage),
-                Marka=_markalist,
+                kampanyalıUrunler = new PageData<DigerUrunler>(currentPosts, totalPostCount, page, postsPerPage),
+                Marka = _markalist,
                 Kategori = context.Katagori.ToList()
             });
         }
